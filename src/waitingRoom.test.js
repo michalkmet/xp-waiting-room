97bb609761f141d8f4b1a7cb0d87374c9349ecbf
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-15 16:40:06 +0200

    UAT1.2 pass

--- src/waitingRoom.test.js
+++ src/waitingRoom.test.js
@@ -1,7 +1,10 @@
 const waitingRoom = require('./waitingRoom');
 
 describe('Story1', () => {
-  it('When given [], it should throw an exception', () => {
+  it('UAT1.1 When given [], it should throw an exception', () => {
     expect(() => waitingRoom([])).toThrow();
+  });
+  it('UAT1.2: When given true, it should throw an exception', () => {
+    expect(() => waitingRoom(true)).toThrow();
   });
 });
